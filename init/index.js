a6cd3109d9a98c9723088822da4b165fffc3f813
Zeeshan Ali Kazmi <sz7185511@gmail.com>
2025-06-07 09:26:20 +0500

    Add project files

--- init/index.js
+++ init/index.js
@@ -16,6 +16,8 @@
     }
     const initDB=async ()=>{
         await Listing.deleteMany({});
+        initData.data = initData.data.map((obj)=>({
+            ...obj,owner:'67f1de95196ff20448fa097b'}));
         await Listing.insertMany(initData.data);
         console.log("data was initialized");
     };
